563a4dccf6ed1a7eddebacbe923084e8605f216b
Mikhail <mikhail.biazverkhi@gmail.com>
2023-09-02 18:57:59 -0400

    Route modifications
    routes/videos.js

--- routes/videos.js
+++ routes/videos.js
@@ -1,115 +1,25 @@
+// Importe le routeur Express et le modèle de tweet de Mongoose
 const router = require("express").Router();
-const Video = require("../database/models/video.model");
-const path = require("path"); // Path (un module natif de Node.js)
-const multer = require("multer"); // Multer (un middleware pour Express.js)
-const fs = require("fs"); // File System (un module natif de Node.js)
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
+// Importe les fonctions du controllers
+const {
+  videoNew,
+  videoList,
+  videoUpload,
+  videoStreaming,
+  upload,
+} = require("../controllers/videos.controller");
 
 //form "upload video"
-router.get("/upload", (req, res) => {
-  res.render("videos/video-form"); // video-form.pug
-});
+router.get("/upload", videoNew);
 
 //show video-list
-router.get("/", async (req, res) => {
-  try {
-    // OK 200
-    const videos = await Video.find({}).exec();
-
-    if (videos.length === 0) {
-      return res.status(404).render("videos/error", {
-        message: "Aucune vidéo trouvée",
-      });
-    }
-
-    res.render("videos/video-list", { videos });
-  } catch (err) {
-    res.status(500).render("videos/error", {
-      message: "Erreur serveur ou problème avec la base de données",
-    });
-  }
-});
+router.get("/", videoList);
 
 //Add Video info to MongoDB and upload video to folder "uploads"
-router.post("/uploadvideo", upload.single("chemin"), async (req, res) => {
-  const { body, file } = req;
-  body.chemin = file.path;
-  body.filename = file.filename;
-
-  console.log(body);
-
-  // Check the file extension here before attempting to save
-  const fileExtension = file.originalname.split(".").pop();
-  if (fileExtension !== "mp4") {
-    return res.status(400).render("videos/error", {
-      message:
-        "Error 400 - Mauvais format du fichier. Seuls les fichiers .mp4 sont autorisés.",
-    });
-  }
-  // Create a new instance of the Video model with MongoDB data
-
-  try {
-    // OK 200
-    const newVideo = new Video(body);
-    await newVideo.save();
-    // res.status(200).json({ message: "Vidéo téléchargée avec succès." });
-    res.redirect("/");
-  } catch (err) {
-    if (err) {
-      return res.status(400).render("videos/error", {
-        message:
-          "Error 500 - Erreur serveur ou problème avec la base de données.",
-      });
-    }
-  }
-});
+router.post("/uploadvideo", upload.single("chemin"), videoUpload);
 
 //Streaming video
-router.get("/stream/:filename", (req, res) => {
-  const filename = path.basename(req.params.filename);
-  const videoPath = path.join(__dirname, "../uploads", filename);
-
-  // Check if the video file exists
-  fs.stat(videoPath, (err, stats) => {
-    if (err || !stats.isFile()) {
-      // If the file doesn't exist, render the error page
-      return res.status(404).render("videos/error", {
-        message: "Error 404 - La vidéo demandée n'est pas trouvée",
-      });
-    }
-
-    // Get the file size
-    const fileSize = stats.size;
-
-    // If the request doesn't contain a Range header, send the entire video file
-    const fileStream = fs.createReadStream(videoPath);
-    res.writeHead(200, {
-      "Content-Length": fileSize,
-      "Content-Type": "video/mp4", // Set the appropriate MIME type for your video
-    });
-
-    fileStream.pipe(res);
-
-    // Handle errors from the file stream
-    fileStream.on("error", (err) => {
-      console.error("Error reading video file:", err);
-      // Respond with a 500 Internal Server Error status and message
-      res.status(500).render("videos/error", {
-        message:
-          "Error 500 - Erreur serveur ou problème avec la lecture du fichier vidéo",
-      });
-    });
-  });
-});
+router.get("/stream/:filename", videoStreaming);
 
 module.exports = router;
